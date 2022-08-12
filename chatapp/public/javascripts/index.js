4a6f278fa5265fe1d99e31ca63d9ad134ddb1dca
ryotaro-rks <ryotaro.seya@rakus.co.jp>
2022-08-12 16:14:55 +0900

    feature/入退室実装

--- chatapp/public/javascripts/index.js
+++ chatapp/public/javascripts/index.js
@@ -3,9 +3,11 @@
 // チャットルームに入室する
 function enter() {
     // 入力されたユーザ名を取得する
-    const userName = '';
+    const userName = $(`#userName`).val();
     // ユーザ名が未入力でないかチェックする
-
+    if (userName === ``){
+        return;
+    }
 
     $('form').submit();
 }
