83412ce54f36ef6fce4ea6cef4f31f8010b139aa
ryotaro-rks <ryotaro.seya@rakus.co.jp>
2022-08-12 18:26:40 +0900

    自分のコメントが目立つように実装

--- chatapp/sockets/publish.js
+++ chatapp/sockets/publish.js
@@ -3,6 +3,7 @@
 module.exports = function (socket, io) {
     // 投稿メッセージを送信する
     socket.on('publishEvent', function (userName, message) {
-        io.sockets.emit(`publishEvent`, userName, message);
+        socket.emit(`publishOwnMessageEvent`, userName, message);
+        socket.broadcast.emit(`publishOtherMessageEvent`, userName, message);
     });
 };
